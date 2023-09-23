12814f2082fc0ef8f45d0ff12a56f8af1133052f
Erez-Shlingbaum <erez.shlingbaum@gmail.com>
2023-09-23 17:14:20 +0300

    More refactors

--- include/symbol.hpp
+++ include/symbol.hpp
@@ -1,29 +1,32 @@
 #pragma once
+
 #include <cstddef>
+#include <memory>
+#include "process.hpp"
 
-namespace OS {
+namespace OS
+{
+    template<typename T>
+    class Symbol
+    {
+    public:
+        Symbol(std::shared_ptr<Process> process, size_t offset) :
+                _process(process), _offset(offset)
+        {}
 
-	template <typename T>
-	class Symbol
-	{
-	public:
-		Symbol(std::shared_ptr<Process> process, size_t offset) :
-			_process(process), _offset(offset)
-		{
-		}
+        Symbol &operator=(T value) const
+        {
+            _process->write_memory<T>(this->_offset, value);
+            return this;
+        }
 
-		Symbol& operator=(T value) const
-		{
-			process.write_memory<T>(this._offset, value);
-			return this;
-		}
+        operator T() const
+        {
+            return _process->read_memory<T>(this->_offset);
+        }
 
-		operator T() const
-		{
-			return process.read_memory<T>(this._offset);
-		}
-	private:
-		const std::shared_ptr<Process> _process;
-		const size_t _offset;
-	};
+    private:
+        std::shared_ptr<Process> _process;
+        size_t _offset;
+    };
 }
